131c5f12288122395279288e64e22c4661c70bf9
tokiya.horikawa <ayikot4423@gmail.com>
2022-11-06 18:02:38 +0900

    feat: View lists in Home page

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # localhost:3000では通信に失敗するためhostをdocker-compose.ymlのコンテナ名に合わせる
+  config.hosts << "api"
 end
