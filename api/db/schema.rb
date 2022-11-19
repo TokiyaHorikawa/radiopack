7a66e3b1eac8921826af1f78eb6a20e6c5add1ee
tokiya.horikawa <ayikot4423@gmail.com>
2022-11-19 23:24:37 +0900

    chore: Create channels

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_124917) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_134820) do
+  create_table "channels", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
