1944064d72fde42b1b7e1bfade404a70306ce2d4
tokiya.horikawa <ayikot4423@gmail.com>
2022-11-20 01:10:26 +0900

    chore: Create model post

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_142520) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_160309) do
   create_table "channels", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
     t.string "url"
@@ -28,10 +28,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_142520) do
     t.index ["channel_id"], name: "index_contents_on_channel_id"
   end
 
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id"
+    t.bigint "content_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["content_id"], name: "index_posts_on_content_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "contents"
+  add_foreign_key "posts", "users"
 end
