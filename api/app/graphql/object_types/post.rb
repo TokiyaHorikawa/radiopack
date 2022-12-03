abe32b6638c9343f7564008bfc6f6b105d2cb220
tokiya.horikawa <ayikot4423@gmail.com>
2022-12-03 22:10:11 +0900

    refactor: Change to module ObjectTypes

--- api/app/graphql/object_types/post.rb
+++ api/app/graphql/object_types/post.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
-class ObjectTypes::Post < Types::BaseObject
-  field :id, ID, null: false
-  field :title, String
-  field :description, String
-  field :user_id, Integer
-  field :content_id, Integer
-  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-  field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+module ObjectTypes
+  class Post < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String
+    field :description, String
+    field :user_id, Integer
+    field :content_id, Integer
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end  
 end
+
